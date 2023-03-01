bb951eaa5f168b062735f2502b81d36471e00cc2
Brandonf55 <brandon.fenk99@gmail.com>
2023-03-01 10:50:11 -0800

    now have work ChatGPT api on the porject.

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+    return(
+        <header>
+            <h1>Welcome to GhatGPT Api you bitch</h1>
+        </header>
+    )
+}
+
+export default Header();
